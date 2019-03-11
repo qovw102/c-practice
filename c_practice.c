dd31ece342d4502d1a817ba7ba892024a038cb0d
Nelson Ye <qovw102@gmail.com>
2019-03-11 22:58:49 +0800

    overflow practice

--- c_practice.c
+++ c_practice.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+//#include<stdlib.h>
 
 int main(void)
 {
-    printf("Hello C\n");
+    int sum,s=32767;
+
+    sum=s+1;
+    printf("sum+1= %d\n",sum);
+    sum=s+2;
+    printf("sum+2= %d\n",sum);
+
+    //system("pause");
     return 0;
 }
