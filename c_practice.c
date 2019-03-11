e7569f77c77c526e69c6b74e9ccd054963c5ef85
Nelson Ye <qovw102@gmail.com>
2019-03-11 23:50:40 +0800

    float practice

--- c_practice.c
+++ c_practice.c
@@ -4,11 +4,14 @@
 int main(void)
 {
     int sum,s=32767;
+    float f=123.4567890123;
 
     sum=s+1;
     printf("sum+1= %d\n",sum);
     sum=s+2;
     printf("sum+2= %d\n",sum);
+    printf("sizeof sum is %d\n",sizeof(sum));
+    printf("float f = %e\n",f);
 
     //system("pause");
     return 0;
