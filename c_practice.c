638ba109e619880d319d2eb29c6439d5c6a85555
Nelson Ye <qovw102@gmail.com>
2019-03-24 22:46:53 +0800

    mod,a++,++a

--- c_practice.c
+++ c_practice.c
@@ -3,15 +3,25 @@
 
 int main(void)
 {
-    int sum,s=32767;
-    float f=123.4567890123;
+    // int sum,s;
+    // float f=123.4567890123;
+    int a=3, b=3;
 
-    sum=s+1;
-    printf("sum+1= %d\n",sum);
-    sum=s+2;
-    printf("sum+2= %d\n",sum);
-    printf("sizeof sum is %d\n",sizeof(sum));
-    printf("float f = %e\n",f);
+    // printf("Please enter a number:\n");
+    // scanf("%d",&s);
+    // sum=s+1;
+    // printf("sum+1= %d\n",sum);
+    // sum=s+2;
+    // printf("sum+2= %d\n",sum);
+    // printf("sizeof sum is %d\n",sizeof(sum));
+    // printf("float f = %e\n",f);
+    // printf("12%4=%d\n", 12%4);
+    printf("a=%d:\n",a);
+    printf("a++=%d\n",a++);
+    printf("a=%d:\n",a);    
+    printf("b=%d\n",b);
+    printf("++b=%d\n",++b);
+    printf("b=%d\n",b);
 
     //system("pause");
     return 0;
