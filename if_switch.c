e0a488c0b50f37dc35182f09a99218b3b83e1dfa
Nelson Ye <qovw102@gmail.com>
2019-03-31 17:31:04 +0800

    if elseif else

---
+++ if_switch.c
@@ -0,0 +1,16 @@
+//if,switch,goto
+#include<stdio.h>
+
+int main(void)
+{
+    int number=0;
+
+    printf("Please enter a number:\n");
+    scanf("%d", &number);
+    if (number>0)
+        printf("Number:%d > 0",number);
+    else
+        printf("Number:%d < 0",number);
+    
+
+}
