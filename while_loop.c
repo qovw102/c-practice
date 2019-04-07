1f0826cfc898d9e06dd4132b8b86badef9861751
Nelson Ye <qovw102@gmail.com>
2019-04-07 23:58:55 +0800

    infinite while loop

--- while_loop.c
+++ while_loop.c
@@ -1,14 +1,24 @@
 //while loop
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 
 int main(void)
 {
-    int i=1,sum=0;
-    while(sum<=100)
+    // int i=1,sum=0;
+    // while(sum<=100)
+    // {
+    //     sum+=i;
+    //     printf("1~%d=%d\n",i,sum);
+    //     i++;
+    // }
+
+    //infinite while loop
+    char ch;
+    while(ch!=113)
     {
-        sum+=i;
-        printf("1~%d=%d\n",i,sum);
-        i++;
+        ch=getch();
+        printf("ASCII of key %c=%d\n",ch,ch);
     }
+    printf("You have press q to exit the loop\n");
 }
